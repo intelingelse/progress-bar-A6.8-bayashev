19b80418d6cc45bb6834a7ac698a710675b0f969
Ashim Bayashev <intelingelse@gmail.com>
2020-07-06 03:55:33 +0600

    fixed issue: current progress bar width was used before the addition

--- js/progress-bar.js
+++ js/progress-bar.js
@@ -1,18 +1,19 @@
 let progressBar = $("#myProgressBar");
-let progressBarWidth = 0;
 let progressWidth = $(".progress").width();
+let currentProgressBarWidth = 0;
+
 
 
 
 $("#plusOne").click(function(){
-    progressBarWidth += progressWidth*0.01;
-    progressBar.width(progressBarWidth + progressWidth*0.01);
+    progressBar.width(currentProgressBarWidth + progressWidth*0.01);
+    currentProgressBarWidth += progressWidth*0.01;
 });
 $("#plusThree").click(function(){
-    progressBarWidth += progressWidth*0.03;
-    progressBar.width(progressBarWidth + progressWidth*0.03);
+    progressBar.width(currentProgressBarWidth + progressWidth*0.03);
+    currentProgressBarWidth += progressWidth*0.03;
 });
 $('#plusSeven').click((function(){
-    progressBarWidth += progressWidth*0.07;
-    progressBar.width(progressBarWidth + progressWidth*0.07);
+    progressBar.width(currentProgressBarWidth + progressWidth*0.07);
+    currentProgressBarWidth = currentProgressBarWidth + progressWidth*0.07;
 }));
