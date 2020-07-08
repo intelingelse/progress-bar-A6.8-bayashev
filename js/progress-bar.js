17eb1e1fd8c313a088d433ba497efd3158e47eaa
Ashim Bayashev <intelingelse@gmail.com>
2020-07-08 18:19:58 +0600

    deleted comments and console logs

--- js/progress-bar.js
+++ js/progress-bar.js
@@ -6,11 +6,8 @@ let currentProgressBarWidth = 0;
 
 
 $("#plusOne").click(function(){
-    // progressBar.width(currentProgressBarWidth + progressWidth*0.01);
-    // currentProgressBarWidth += progressWidth*0.01;
     currentProgressBarWidth+=1;
     progressBar.width(`${currentProgressBarWidth}%`);
-    console.log(currentProgressBarWidth);
 });
 $("#plusThree").click(function(){
     currentProgressBarWidth+=3;
